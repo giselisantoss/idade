c9a4421846897e7860a6fd97e230fc6ca0f60e8d
A PONTEW <giselisantos2019@gmail.com>
2022-03-18 20:03:59 -0300

    codigo completo

---
+++ app.js
@@ -0,0 +1,17 @@
+function entrar(){
+    var idade = parseInt(document.getElementById("idade").value);
+
+    if(idade<18){
+        alert(`Voce tem ${idade} anos, menor de idade não pode entra`);
+    } else {
+
+        var idade
+        if(idade>18){
+            alert("Voce e maior de idade");
+        } else {
+            
+        }
+            
+        
+    }
+}
